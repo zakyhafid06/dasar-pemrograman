1426a3fd1d2702f3dca0affadd11160b9fec4cdc
zakyhafid98 <zakyhafid06@gmail.com>
2025-10-04 12:55:30 +0700

    buat kalkulator sederhana

---
+++ kalkulatorsederhana.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a=6;
+int b=2;
+
+cout<<"===================="<<endl;
+cout<<"kalkulator sederhana"<<endl;
+cout<<"===================="<<endl;
+cout<<"dengan a = 6 dan b = 2, maka hasilnya : "<<endl;
+cout<<"1. penjumlahan = "<<(a+b)<<endl;
+cout<<"2. pengurangan = "<<(a-b)<<endl;
+cout<<"3. perkalian   = "<<(a*b)<<endl;
+cout<<"4. pembagian = "<<(a/b)<<endl;
+
+}
